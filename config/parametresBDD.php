96067a2fd99b8e0639e1e3458bd37c18a2386409
davidlachambre <david.tim.12345@gmail.com>
2015-12-11 11:01:33 -0500

    ajout du fichier de config de la BDD

---
+++ config/parametresBDD.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file     mesParametresBD.php
+ * @author   David Lachambre
+ * @version  1.0
+ * @date     2015-12-10
+ * @brief    Définit les constantes de configuration de l'application.   
+ */
+
+/**
+ * @brief HOTE constante qui définit l'adresse de l'hote
+ */
+define("HOTE", "montreart.net");
+
+/**
+ * @brief UTILISATEUR constante qui définit l'utilisateur
+ */
+define("UTILISATEUR", "administrateur");
+
+/**
+ * @brief UTILISATEUR constante qui définit le mot de passe
+ */
+define("PASS", "Bd2cPCoC");
+
+/**
+ * @brief BDD constante qui définit la base de donnée
+ */
+define("BDD", "montreart");
+
+?>
