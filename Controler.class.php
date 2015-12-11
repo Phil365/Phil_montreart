23b26b5c4c6f15d182928a8a37ea539bd16d0e95
davidlachambre <david.tim.12345@gmail.com>
2015-12-11 10:58:40 -0500

    Modification du fichier Controler.class

--- Controler.class.php
+++ Controler.class.php
@@ -4,40 +4,66 @@
  * Gère les requêtes HTTP
  * 
  * @author Jonathan Martel
- * @version 1.0
- * @update 2013-12-10
+ * @author David Lachambre
+ * @version 1.1
+ * @update 2015-12-10
  * @license Creative Commons BY-NC 3.0 (Licence Creative Commons Attribution - Pas d’utilisation commerciale 3.0 non transposé)
  * @license http://creativecommons.org/licenses/by-nc/3.0/deed.fr
  * 
  */
 
-class Controler 
-{
-	
-		/**
-		 * Traite la requête
-		 * @return void
-		 */
-		public function gerer()
-		{
-			
-			switch ($_GET['requete']) {
-				case 'accueil':
-					$this->accueil();
-					break;
-				default:
-					$this->accueil();
-					break;
-			}
-		}
-		private function accueil()
-		{
-			$oVue = new Vue();
-			$oVue->afficheAccueil();
-		}
-		// Placer les méthodes du controleur.
-		
-		
+class Controler {
+    
+    private $oVue;
+    
+    public function __construct() {
+        
+        $this->oVue = new Vue();
+    }
+    /**
+     * Traite la requête
+     * @return void
+     */
+    public function gerer() {
+        
+        $this->meta();
+        $this->entete();
+        switch ($_GET['r']) {//requête
+            case 'accueil':
+                $this->accueil();
+                break;
+            case 'oeuvre':
+                $this->oeuvre();
+                break;
+            default:
+                $this->accueil();
+                break;
+        }
+        $this->piedPage();
+    }
+    private function meta() {
+        
+        $this->oVue->afficheMeta();
+    }
+    private function entete() {
+        
+        $this->oVue->afficheEntete();
+    }
+    private function accueil() {
+        
+        $this->oVue->afficheAccueil();
+    }
+    private function oeuvre() {
+        
+        $oeuvre = new Oeuvre();
+        $oeuvre->setData(1, "titre test", 1234, "45.554", "56.534645", null, null, "adresse test", "description test", 1, 1, 1, 1, ["photo1", "photo2"], ["commentaire1", "commentaire2"]);
+        $this->oVue->affichePageOeuvre($oeuvre);
+    }
+    private function piedPage() {
+        
+        $this->oVue->affichePiedPage();
+    }
+    // Placer les méthodes du controleur.
 }
 ?>
 
