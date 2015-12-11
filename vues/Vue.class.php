d423942278e161511c5fdd7464e5675731c4e597
davidlachambre <david.tim.12345@gmail.com>
2015-12-11 11:01:17 -0500

    Modification du la vue
    - Ajout de l'affichage de l'oeuvre
    - Séparation des éléments pour écrire le méta, le header et le footer
    séparément du corps de la page

--- vues/Vue.class.php
+++ vues/Vue.class.php
@@ -14,17 +14,12 @@
 
 class Vue {
 
-	/**
-	 * Affiche la page d'accueil 
-	 * @access public
-	 * 
-	 */
-	public function afficheAccueil() {
-		?>
-		<!DOCTYPE html>
-<html lang="fr">
+    public function afficheMeta() {
+    ?>
+    <!DOCTYPE html>
+    <html lang="fr">
 	<head>
-		<title>Mon simple MVC</title>
+		<title>MontréArt</title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="description" content="">
@@ -38,26 +33,59 @@ class Vue {
 		<script src="./js/plugins.js"></script>
 		<script src="./js/main.js"></script>
 	</head>
-
-	<body>
+    <?php
+    }
+    public function afficheEntete() {
+    ?>
+    <body>
+        <header>
+            <div>HEADER</div>
+        </header>
+    <?php
+    }
+	/**
+	 * Affiche la page d'accueil 
+	 * @access public
+	 * 
+	 */
+	public function afficheAccueil() {
+?>
+	
 		<article>
-			<h1>Bienvenue sur Simple MVC Structure </h1>
-			<p>Simple MVC Structure  n'est pas un framework, mais seulement une structure de base qui permet de monter un MVC rapidement en php. 
-				Il suffit de forker le <a href="#">dépot Github</a> et de dupliquer les classes vues et modele afin d'en disposer à votre convenance.</p>
+			<h1>MontréArt</h1>
+			<p>Site en construction</p>
 		</article>
 		
-			<div id="footer">
-				Certains droits réservés @ Jonathan Martel (2013)<br>
-				Sous licence Creative Commons (BY-NC 3.0)
-			</div>
-		</div>	
+        <div id="footer">
+        </div>
 	</body>
 </html>
 
-		<?php
-		
+<?php	
 	}
-	
-
+    public function affichePageOeuvre($oeuvre) {
+?>        
+		<article>
+			<h1>Page d'une oeuvre</h1>
+			<p>
+            <?php
+                var_dump($oeuvre->getData());
+            ?>
+            </p>
+		</article>
+		
+        <div id="footer">
+        </div>
+<?php
+    }
+    public function affichePiedPage() {
+    ?>
+                <footer>
+                    <div>FOOTER</div>
+                </footer>
+            </body>
+        </html>
+    <?php
+    }
 }
 ?>
