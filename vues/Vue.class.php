04a55be408fa93fd1280734f000e29963fa5137b
davidlachambre <david.tim.12345@gmail.com>
2015-12-14 01:05:13 -0500

    modification de la vue pour l'affichage de la page des oeuvres

--- vues/Vue.class.php
+++ vues/Vue.class.php
@@ -10,19 +10,23 @@
  * @license http://creativecommons.org/licenses/by-nc/3.0/deed.fr
  * 
  */
-
+header('Content-Type: text/html; charset=utf-8');//Affichage du UTF-8 par PHP.
 
 class Vue {
 
-    public function afficheMeta() {
+    /**
+	 * Affiche le meta de la page et ouvre les balises html
+	 * @access public
+	 * 
+	 */
+    public function afficheMeta($meta) {
     ?>
     <!DOCTYPE html>
     <html lang="fr">
 	<head>
-		<title>MontréArt</title>
+		<title><?php echo $meta["titre"] ?></title>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="description" content="">
+		<meta name="description" content="<?php echo $meta["description"] ?>">
 		<meta name="viewport" content="width=device-width">
 		
 		<link rel="stylesheet" href="./css/normalize.css" type="text/css" media="screen">
@@ -35,56 +39,167 @@ class Vue {
 	</head>
     <?php
     }
-    public function afficheEntete() {
+    
+    /**
+	 * Affiche l'entête des pages
+	 * @access public
+	 * 
+	 */
+    public function afficheEntete($pageActuelle) {
     ?>
-    <body>
-        <header>
-            <div>HEADER</div>
-        </header>
+
+        <body>
+            <header>
+                <div>HEADER</div>
+            </header>
+
     <?php
     }
+    
 	/**
 	 * Affiche la page d'accueil 
 	 * @access public
 	 * 
 	 */
 	public function afficheAccueil() {
-?>
-	
-		<article>
-			<h1>MontréArt</h1>
-			<p>Site en construction</p>
-		</article>
-		
-        <div id="footer">
-        </div>
-	</body>
-</html>
+    ?>
 
-<?php	
+            <article>
+                <h1>MontréArt</h1>
+                <p>Site en construction</p>
+            </article>
+
+            <div id="footer">
+            </div>
+        </body>
+    </html>
+
+    <?php	
 	}
-    public function affichePageOeuvre($oeuvre) {
-?>        
-		<article>
-			<h1>Page d'une oeuvre</h1>
-			<p>
-            <?php
-                var_dump($oeuvre->getData());
-            ?>
-            </p>
-		</article>
-		
-        <div id="footer">
-        </div>
-<?php
+    
+    /**
+	 * Affiche la page d'une oeuvre
+	 * @access public
+	 * 
+	 */
+    public function affichePageOeuvre($oeuvre, $commentaires, $photos) {
+        if (empty($oeuvre)) {
+            echo "Cette oeuvre n'a pas été trouvée dans la base de données";
+        }
+        else {
+//            var_dump($oeuvre);
+//            var_dump($commentaires);
+//            var_dump($photos);
+    ?>        
+            <article>
+                <h1><?php echo $oeuvre["titre"]; ?></h1>
+                <p>
+                <?php
+                    if ($photos) {
+                        for ($i = 0; $i < count($photos); $i++) {
+                            $imgPhoto = $photos[$i]['image'];
+                            echo "<img src = '../images/$imgPhoto'><br>";
+                        }
+                    }
+                    else {
+                        echo "Aucun commentaire";
+                    }
+                    
+                    if ($oeuvre["description"]) {
+                        echo $oeuvre["description"];
+                    }
+                    echo "</p>";
+
+                    echo "<p>";
+                    if ($oeuvre["nomArtiste"]) {
+                        echo "Artiste : ";
+                        if ($oeuvre["prenomArtiste"]) {
+                            echo $oeuvre["prenomArtiste"]." ";
+                        }
+                        echo $oeuvre["nomArtiste"]."<br>";
+                    }
+                    if ($oeuvre["nomCollection"]) {
+                        echo "Collection : ".$oeuvre["nomCollection"]."<br>";
+                    }
+                    if ($oeuvre["nomCategorie"]) {
+                        echo "Catégorie : ".$oeuvre["nomCategorie"]."<br>";
+                    }
+                ?>
+                </p>
+                    <h2>Emplacement</h2>
+                <p>
+                <?php
+                    if ($oeuvre["parc"]) {
+                        echo "Parc : ".$oeuvre["parc"]."<br>";
+                    }
+                    if ($oeuvre["batiment"]) {
+                        echo "Bâtiment : ".$oeuvre["batiment"]."<br>";
+                    }
+                    if ($oeuvre["adresse"]) {
+                        echo "Adresse : ".$oeuvre["adresse"]."<br>";
+                    }
+                    echo "</p>";
+                   
+                    echo "<h2>Commentaires</h2>";
+                    echo "<p>";
+                    if ($commentaires) {
+                        for ($i = 0; $i < count($commentaires); $i++) {
+                            switch ($commentaires[$i]['voteCommentaire']) {
+                                case 1:
+                                    $imgVote = "etoiles_1.png";
+                                    break;
+                                case 2:
+                                    $imgVote = "etoiles_2.png";
+                                    break;
+                                case 3:
+                                    $imgVote = "etoiles_3.png";
+                                    break;
+                                case 4:
+                                    $imgVote = "etoiles_4.png";
+                                    break;
+                                case 5:
+                                    $imgVote = "etoiles_5.png";
+                                    break;
+                                default:
+                                    $imgVote = "etoiles_0.png";
+                                    break;
+                            }
+                            $imgPhoto = $commentaires[$i]['photoProfil'];
+
+                            echo "<img src = '../images/$imgPhoto'><br>";
+                            echo $commentaires[$i]["nomUsager"]."<br>";
+                            echo "<img src = '../images/$imgVote'><br>";
+                            echo $commentaires[$i]["texteCommentaire"]."<br>";
+                        }
+                    }
+                    else {
+                        echo "Aucun commentaire";
+                    }
+                    echo "</p>";
+                ?>
+            </article>
+
+            <div id="footer">
+            </div>
+
+    <?php
+        }
     }
+    
+    /**
+	 * Affiche le pied de page et ferme les balises html
+	 * @access public
+	 * 
+	 */
     public function affichePiedPage() {
     ?>
-                <footer>
-                    <div>FOOTER</div>
-                </footer>
-            </body>
-        </html>
+
+            <footer>
+                <div>FOOTER</div>
+            </footer>
+        </body>
+    </html>
+
     <?php
     }
 }
