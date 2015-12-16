de80df0595247fb2c2559f27f19a59781f47c6a1
davidlachambre <david.tim.12345@gmail.com>
2015-12-16 17:21:33 -0500

    intégration des éléments html de la vue accueil

--- vues/VueAccueil.class.php
+++ vues/VueAccueil.class.php
@@ -16,18 +16,47 @@ class VueAccueil extends Vue {
     }
     
     /**
-	 * Affiche le meta de la page et ouvre les balises html
-	 * @access public
-	 * 
-	 */
+    * @brief Méthode qui affiche le corps du document HTML
+    * @access public
+    * @return void
+    */
     public function afficherBody() {
     ?>
-        <body>
-            <article>
-                <h1>MontréArt</h1>
-                <p>Site en construction</p>
-            </article>
-        </body>
+        <div class="dummy"><!--    Ne mettez rien ici--></div>
+
+            <div class="section1">
+
+               section 1     Participe au projet
+            </div> 
+            <div class="section2">
+                    slider section2
+            </div>
+
+            <div class="section3">
+
+                    section3 Oeuvre en Vedette
+            </div>
+            <div class="section4">
+                    A propos section4
+            </div>
+             <div class="section5">
+                    sponsors section5
+            </div>
+
+              <div id="div_bgform">
+            <div id="div_form">
+                <!-- Formulaire login -->
+                <form action="#" id="formlogin" method="post" name="formlogin">
+                    <button id="fermer" onclick ="fermer()">X</button>
+                <h2>Connectez vous</h2>
+
+                    <input id="nomutilisateur" name="nomutilisateur" placeholder="Votre identifiant" type="text">
+                    <input id="motpasse" name="motpasse" placeholder="Mot de passe" type="password">
+
+                    <button onclick="validerform()" class="submit" id="submit">Envoyer</button>
+                    </form>
+                </div>
+        </div>
     <?php
     }
 }
