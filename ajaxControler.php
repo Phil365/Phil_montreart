6a0b0c1e2deaf898681855c80e97e5c59d5af025
davidlachambre <davidlach@hotmail.com>
2016-01-14 09:18:11 -0500

    controleur à jour pour fonctionnement Google Map

--- ajaxControler.php
+++ ajaxControler.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Controlleur AJAX. Ce fichier est la porte d'entrée des requêtes AJAX (XHR)
  * @author Jonathan Martel
@@ -10,10 +9,37 @@
  * 
  */
 
-	require_once("./config.php");
+require_once("./config.php");
 	
-	
-	// Mettre ici le code de gestion de la requête AJAX
-	
+// Mettre ici le code de gestion de la requête AJAX
+
+// Start XML file, create parent node
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+$oeuvre = new Oeuvre();
+$infoOeuvre = $oeuvre->getAllOeuvreWithPhoto();
+
+header("Content-type: text/xml");
+
+// Iterate through the rows, adding XML nodes for each
+
+  // ADD TO XML DOCUMENT NODE
+for ($i = 0; $i < count($infoOeuvre); $i++) {
+    $node = $dom->createElement("marker");
+    $newnode = $parnode->appendChild($node);
+    $newnode->setAttribute("name",$infoOeuvre[$i]["titre"]);
+    //$newnode->setAttribute("address", $row['adresse']);
+    $newnode->setAttribute("lat", $infoOeuvre[$i]["latitude"]);
+    $newnode->setAttribute("lng", $infoOeuvre[$i]["longitude"]); 
+    $newnode->setAttribute("photo", $infoOeuvre[$i]["image"]);   
+$newnode->setAttribute("urlTest", "http://localhost?r=oeuvre&o=".$infoOeuvre[$i]["idOeuvre"]);
+  $newnode->setAttribute("url", "http://montreart.net?r=oeuvre&o=".$infoOeuvre[$i]["idOeuvre"]);
+}
+//}
+
+echo $dom->saveXML();
 
 ?>
